e842b9c7055442329123224ae3fe158094b41af2
AyaSoliman99 <ayasoliam3799@gmail.com>
2022-05-07 12:01:57 +0200

    add functionality

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,21 @@
-import "./Meme.css"
+import "./Meme.css";
+import { useState } from "react";
+import memeData from "../memeData";
 export default function Meme() {
+    let url;
+    function getImgHandler() {
+        let math = parseInt(Math.random() *(memeData.data.memes.length));
+    //    return memeData.data.memes[math].url
+        url = memeData.data.memes[math].url;
+    console.log(url);
+    }
     return (<div className="meme">
-        <form>
+        <div className="form">
             <div className="inputs">
                 <input type="text" placeholder="Top Text" />
                 <input type="text" placeholder="Bottom Text" />
             </div>
-            <button className="get-img">Get a new meme image 🖼</button>
-        </form>
+            <button className="get-img" onClick={getImgHandler}>Get a new meme image 🖼</button>
+        </div>
     </div>)
 }
